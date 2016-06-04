d5c1b424d426524cd21c592f374d822ff1c10f9e
Meirion Hughes <mhughes@geologging.com>
2016-06-04 15:03:21 +0100

    remove default rules.

--- source/index.ts
+++ source/index.ts
@@ -10,11 +10,4 @@ import {RequireRule} from './require'
 export * from './template'
 export * from './require'
 export * from './slot'
-
-export var DefaultRules = [
-    new SelfCloseRule(),
-    new ParserRule(),
-    new TemplateRule(),
-    new SlotRule(),
-    new RequireRule(),
-]
+export * from './linter'
