dea13e10cc9a8dbed02e5093299febb022d1df6c
Meirion Hughes <mhughes@geologging.com>
2016-08-04 07:53:19 +0100

    add failing spec for #71

--- spec/failing.spec.ts
+++ spec/failing.spec.ts
@@ -10,28 +10,27 @@ import {initialize} from 'aurelia-pal-nodejs';
 initialize();
 
 describe("Failing Scenarios", () => {
-    //uncomment, add your example and what you expect. 
-    /*it("some test that fails", (done) => {
-        var config: Config = new Config();
-        var linter: AureliaLinter = new AureliaLinter(config);
-        var html = `<template></etemps> <!-- oops! -->`
-        linter.lint(html)
-            .then((issues) => {                   
-                expect(issues.length).toBe(0);
-                done();
-            });
-    });*/
+  //uncomment, add your example and what you expect. 
+  /*it("some test that fails", (done) => {
+      var config: Config = new Config();
+      var linter: AureliaLinter = new AureliaLinter(config);
+      var html = `<template></etemps> <!-- oops! -->`
+      linter.lint(html)
+          .then((issues) => {                   
+              expect(issues.length).toBe(0);
+              done();
+          });
+  });*/
 
   //#67
   it("support custom elements", (done) => {
-    let itemViewModel = `
+    let itemCustomElement = `
     import {bindable} from "aurelia-templating";
     export class ItemCustomElement {
         @bindable value: string;
     }`;
 
     let pageViewModel = `
-    import {Item} from './item
     export class Foo {
       fooValue:number;
     }`
@@ -45,13 +44,43 @@ describe("Failing Scenarios", () => {
     let reflection = new Reflection();
     let rule = new BindingRule(reflection);
     let linter = new Linter([rule]);
-    reflection.add("./item.ts", itemViewModel);
+    reflection.add("./item.ts", itemCustomElement);
     reflection.add("./page.ts", pageViewModel);
     linter.lint(pageView, "./page.html")
       .then((issues) => {
         try {
           expect(issues.length).toBe(1);
           expect(issues[0].message).toBe("cannot find 'bad' in type 'ItemCustomElement'")
+        }
+        catch (err) { fail(err); }
+        finally { done(); }
+      })
+  });
+
+  // 
+  it("binding to method should cause no exception", (done) => {
+
+    let pageViewModel = `
+    export class Page {
+      value:number;
+      public submit) {        
+      }
+    }`
+
+    let pageView = `
+    <template>
+      \${value}
+      <form role="form" submit.delegate="submit()"></form>
+    </template>`
+
+    let reflection = new Reflection();
+    let rule = new BindingRule(reflection, {reportExceptions:true});
+    let linter = new Linter([rule]);
+    reflection.add("./page.ts", pageViewModel);
+    linter.lint(pageView, "./page.html")
+      .then((issues) => {
+        try {
+          expect(issues.length).toBe(0);
         }
         catch (err) { fail(err); }
         finally { done(); }
