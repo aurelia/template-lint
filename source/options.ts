79d099d241c28ef1ec8e579c5f295ad112c577c1
Meirion Hughes <crakinshot@yahoo.com>
2016-12-14 21:49:10 +0000

    chore(options): clean up

--- source/options.ts
+++ source/options.ts
@@ -1,17 +1,20 @@
-export type obsoleteElement = { elmt: string, msg?: string };
-export type obsoleteAttribute = { attr: string, elmt?: string, msg?: string };
-export type conflictingAttributeSet = { attrs: string[], msg?: string };
+export type ObsoleteElement = { elmt: string, msg?: string };
+export type ObsoleteAttribute = { attr: string, elmt?: string, msg?: string };
+export type ConflictingAttributeSet = { attrs: string[], msg?: string };
 
 export class Options {
   "process" = true;
-  "obsolete-elements" = new Array<obsoleteElement>(
-    { elmt: 'content', msg: 'use slot instead' }
-  );
-  "obsolete-attributes" = new Array<obsoleteAttribute>();
 
   "report-html-self-close" = true;
   "report-html-obsolete-elements" = true;
   "report-html-obsolete-attributes" = true;
   "report-html-require-not-found" = false;
   "report-html-require-view-when-viewmodel-exists" = true;
+
+  "obsolete-elements" = new Array<ObsoleteElement>(
+    { elmt: 'content', msg: 'use slot instead' }
+  );
+
+  "obsolete-attributes" = new Array<ObsoleteAttribute>(    
+  );
 }
